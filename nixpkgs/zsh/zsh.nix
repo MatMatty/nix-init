c5fc0919f716a60f4cf045b0aba3671d851b868f
MatMatty <43439252+MatMatty@users.noreply.github.com>
2022-11-29 16:41:12 +0100

    Update zsh.nix

--- nixpkgs/zsh/zsh.nix
+++ nixpkgs/zsh/zsh.nix
@@ -5,6 +5,10 @@ unstable = import <nixos-unstable> {config = {allowUnfree = true; }; };
 in
 {
     enable = true;
+    home.sessionVariables = {
+          PGDATA = "$HOME/postgres_data"
+          PGHOST = "/tmp"
+        };
     shellAliases = {
         epita_gcc="gcc -Wall -Wextra -Werror -pedantic -std=c99";
         epita_auto_git="$AFS_DIR/nix-init/git_auto.sh";
